f37f06855ff8598847028197c41f9fc54c040683
Pawel Obrycki <pawel.obrycki@gmail.com>
2026-05-18 20:06:05 -0700

    Module 6.2 numpy

--- module6_knn-regr.py
+++ module6_knn-regr.py
@@ -1,27 +1,29 @@
 import numpy as np
 
-
+# allocate storage
 class KNNRegression:
     def __init__(self, n):
         self.n = n
-        self.x = np.zeros(n, dtype=float)
-        self.y = np.zeros(n, dtype=float)
-        self.count = 0
+        self.x = np.zeros(n, dtype=float) # pre-sized array to enable vectorized operations
+        self.y = np.zeros(n, dtype=float)# pre-sized array to enable vectorized operations
+        self.count = 0 #counter
 
+    # add points
     def insert(self, x, y):
         self.x[self.count] = x
         self.y[self.count] = y
         self.count += 1
 
+    #actual knn regression function
     def predict(self, x_query, k):
         if k > self.n:
-            return None
+            return None # exit if not enough neighbours
 
-        distances = np.abs(self.x - x_query)
-        nearest_indices = np.argpartition(distances, k - 1)[:k]
-        return np.mean(self.y[nearest_indices])
+        distances = np.abs(self.x - x_query) #calc distance
+        nearest_indices = np.argpartition(distances, k - 1)[:k] # pick indices of the k smallest distances
+        return np.mean(self.y[nearest_indices]) # average the y values of k neighours
 
-
+# main function to get input from user
 def main():
     n = int(input("Enter a positive integer N: "))
     k = int(input("Enter a positive integer k: "))
